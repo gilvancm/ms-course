020c75cdb754f9c60be55fde1035f1c468dc9e38
Gilvan Muniz <gilvanmunizpa@gmail.com>
2022-12-07 12:07:41 -0300

    Feign Cleint comunicação entre os projetos

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,9 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//obs ainda não é microserviço e só uma cominicação entre os projetos
-//sendo hadcode
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
@@ -13,3 +13,14 @@ public class HrPayrollApplication {
 	}
 
 }
+
+/*
+vamos incluir o primeirocomponente de Spring Clound 
+para a comunicação do projeto é o Feign e ele vai tar 
+integrada como outras ferramentas do Spring Clound
+vamosacresentar na dependencia Feign do Spring Clound Rounting que é OpenFeign
+para Feing funcionar vamos ter de criar uma interface com as assinaturas
+*/
+
+//obs ainda não é microserviço e só uma cominicação entre os projetos
+//sendo hadcode
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,62 +1,26 @@
 package com.devsuperior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 //registro o componente no sprint boot
 //aminha url
 
 @Service
 public class PaymentService {
-	//pegar o valor do caminho lá na propriedade depois guardo num atributo da classe abaixo
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	
 	//injeção de dependencia
 	@Autowired
-	private RestTemplate restTemplate;
-	
+	private WorkerFeignClient workerFeignClient;
+
 	public Payment getPayment(long workerId, int days) {
-	//como fazer uma requisição para um API externo
-	//temos que criar uma classe worker neste projeto ai temos uma declaração do worker
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id",""+ workerId); //"" para converter para String
-		////caminho da requisição, passar os parametros uriVariables
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
-		//preenchimento dinamico pegando no projeto worker
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
