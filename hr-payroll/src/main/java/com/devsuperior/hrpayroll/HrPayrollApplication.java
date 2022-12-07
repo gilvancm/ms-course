adc155248fdfe36219cb43243b1f0eb9f8fa1a8a
Gilvan Muniz <gilvanmunizpa@gmail.com>
2022-12-07 10:38:45 -0300

    RestTemplate uma comunicação entre os projetos não microservice ainda

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -3,6 +3,8 @@ package com.devsuperior.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//obs ainda não é microserviço e só uma cominicação entre os projetos
+//sendo hadcode
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,18 +1,39 @@
 package com.devsuperior.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
 
 //registro o componente no sprint boot
 //aminha url
 
 @Service
 public class PaymentService {
+	//pegar o valor do caminho lá na propriedade depois guardo num atributo da classe abaixo
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	
+	//injeção de dependencia
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public Payment getPayment(long workerId, int days) {
-		//pagamento mocado MOCK
-		return new Payment("Bob", 200.0, days);
+	//como fazer uma requisição para um API externo
+	//temos que criar uma classe worker neste projeto ai temos uma declaração do worker
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id",""+ workerId); //"" para converter para String
+		////caminho da requisição, passar os parametros uriVariables
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		//preenchimento dinamico pegando no projeto worker
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 	
