430879aeb82aed02c39670ee21ed617ab0049782
Gilvan Muniz <gilvanmunizpa@gmail.com>
2022-12-07 22:51:20 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/main/java/com/devsuperior/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.devsuperior.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
+//anotação de dependencia Eureka Server
