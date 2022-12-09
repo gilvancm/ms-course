43f860a94268bfba1c0ff468765c11c486e31aab
Gilvan Muniz <gilvanmunizpa@gmail.com>
2022-12-09 14:48:13 -0300

    Project hr-api-gateway-zuul created gateway é portal

---
+++ hr-api-gatewary-zuul/src/main/java/com/devsuperior/hrapigatewaryzuul/HrApiGatewaryZuulApplication.java
@@ -0,0 +1,19 @@
+package com.devsuperior.hrapigatewaryzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*dependencia
+Spring Web
+Spring Clound Discovery - Eureka Discovery Client
+Spring Clound Routing - Zuul[Maintenance]
+*/
+
+
+@SpringBootApplication
+public class HrApiGatewaryZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewaryZuulApplication.class, args);
+	}
+
+}
