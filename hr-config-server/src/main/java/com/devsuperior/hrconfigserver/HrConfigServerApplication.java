6bf3b1f6d5f0d9a192acefd4bc14d45325915130
Gilvan Muniz <gilvanmunizpa@gmail.com>
2022-12-11 16:53:35 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 //temos que incluir a dependencia Config Sever
 
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
